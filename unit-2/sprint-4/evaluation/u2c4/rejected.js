d78692848b38d48bd6e488900bf9551b6a068f94
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-03 03:05:51 +0530

    Create rejected.js

---
+++ unit-2/sprint-4/evaluation/u2c4/rejected.js
@@ -0,0 +1,28 @@
+// All the Code for the Rejected page goes here
+
+let arr4 = JSON.parse(localStorage.getItem("admission-rejected"))
+
+arr4.forEach(function (elem) {
+
+    let tr = document.createElement("tr")
+    document.querySelector("tbody").append(tr)
+
+    let ntd = document.createElement("td")
+    ntd.innerText = elem.name;
+
+    let etd = document.createElement("td")
+    etd.innerText = elem.email;
+
+    let ctd = document.createElement("td")
+    ctd.innerText = elem.course;
+
+    let gtd = document.createElement("td")
+    gtd.innerText = elem.gender;
+
+    let ptd = document.createElement("td")
+    ptd.innerText = elem.phone;
+
+    tr.append(ntd, etd, ctd, gtd, ptd);
+
+
+})
