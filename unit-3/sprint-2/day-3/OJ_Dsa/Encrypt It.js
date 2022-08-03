9a3e32fbbd4161e36275d4ded5b0ee88228c3966
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-04 00:13:09 +0530

    Create Encrypt It.js

---
+++ unit-3/sprint-2/day-3/OJ_Dsa/Encrypt It.js
@@ -0,0 +1,7 @@
+// Encryption is the process of converting a plaintext message into ciphertext which can be decoded back into the original message. An encryption algorithm along with a key is used in the encryption and decryption of data. There are several types of data encryptions which form the basis of network security. Encryption schemes are based on block or stream ciphers.
+
+// Today, your task is to encrypt and print the encrypted string. You will be given a single line which contains alphabets, numbers and symbols, followed by a number K. You need to encrypt it in a way such that every character in the string is rotated by the given fixed number K.
+
+// Only encrypt Alphabets and Numbers. (A-Z, a-z, and 0-9) . All Symbols, such as - , ; %, should remain unencrypted.
+
+// Look at the sample test case for better understanding.
