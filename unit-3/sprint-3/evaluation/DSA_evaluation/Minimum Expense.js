684f0c569dc21d9d6a6b36506bac7aa11b48e7aa
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-04 00:15:28 +0530

    Create Minimum Expense.js

---
+++ unit-3/sprint-3/evaluation/DSA_evaluation/Minimum Expense.js
@@ -0,0 +1,7 @@
+// You are given an array ofNintegers, such that it denotes the number of rupees spent byStanleyforNdays
+
+// You have to find the minimum amount of money he has spent onKconsecutive days
+
+// Declare a variable with the nameKevin, and find the minimum amount of money that he has spent during a consecutive period ofKdays
+
+// Note : The days in which he earns more money that what he spends, are given as negative numbers in the array
