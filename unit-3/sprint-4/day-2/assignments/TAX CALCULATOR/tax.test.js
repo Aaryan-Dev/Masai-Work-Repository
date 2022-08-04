e8812f962f34a8101c51b99ee118522b37436748
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-05 03:06:24 +0530

    Create tax.test.js

---
+++ unit-3/sprint-4/day-2/assignments/TAX CALCULATOR/tax.test.js
@@ -0,0 +1,48 @@
+
+
+// - Below 2,50,000 no tax
+// - Amount earned between 2,50,000 and 5,00,000 - 10% of the amount earned in this slab
+// - Amount earned between 5,00,000 and 10,00,000 - 20% of the amount earned in this slab
+// - Amount earned above 10,00,000 - 30% of the amount earned in this slab
+
+
+let tax = (val) => {
+
+  if (val <250000) {
+   
+    return "no tax"
+    
+  }
+  else if (val >= 250000 && val <500000 ) {
+    return Math.round(val / 10);
+    
+  } else if (val >= 500000 && val <1000000 ) {
+    
+    return Math.round(val / 20); 
+
+  } else
+    
+    return Math.round(val / 30);
+
+  }
+
+  
+
+
+
+test('tax', () => {
+  expect(tax(35000)).toBe("no tax");
+});
+
+test('tax', () => {
+  expect(tax(300000)).toBe(30000);
+});
+
+test('tax', () => {
+  expect(tax(700000)).toBe( Math.round(700000/20));
+});
+
+test('tax', () => {
+  expect(tax(15000000)).toBe(1500000/3);
+});
+
