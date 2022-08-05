56d86706818570d57636de2a0fffbd49e1358a05
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-06 00:50:25 +0530

    Create Sherlock and his queries.js

---
+++ unit-3/sprint-4/evaluation/DSA_evaluation/Sherlock and his queries.js
@@ -0,0 +1,11 @@
+// Sherlock has a matrix of dimension [nxm]. All the elements in this matrix are zero (Initialised as 0).
+
+// Sherlock has q queries to execute on the matrix. Each query has two integers i.e, the type of query and the index.
+
+// So if the type is 0, the index represents the index of row. Similarly, if the type is 1, the index represents the index of the column.
+
+// Sherlock has to increase the value by 1 of all the elements that are present in that row or column depending on the type and index.
+
+// In the end, after executing all queries, Sherlock is curious and wants to find the number of elements of the matrix which are odd (not divisible by 2).
+
+// Note - 0-based indexing is followed here.
