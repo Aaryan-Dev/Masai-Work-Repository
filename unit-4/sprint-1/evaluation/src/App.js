70876812784009bf9a5cbfff0df2fe2a18d56b0e
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-06 00:51:20 +0530

    Create App.js

---
+++ unit-4/sprint-1/evaluation/src/App.js
@@ -0,0 +1,11 @@
+import List from "./Components/List";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Limited List</h1>
+      <List />
+    </div>
+  );
+}
