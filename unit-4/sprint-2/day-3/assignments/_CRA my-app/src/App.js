19f33f9f590cd26ecb83e3c911ef7c22b73c2e47
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-09 00:19:17 +0530

    Create App.js

---
+++ unit-4/sprint-2/day-3/assignments/_CRA my-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > */}
+        <p>created create a react app project using CRA</p>
+        <div>
+          <input type="text" />
+        </div>
+
+        {/* </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
