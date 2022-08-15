787c215a8fe4dd829fd1bc5e686c0a82ec040ab3
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-16 00:14:58 +0530

    Create signup.jsx

---
+++ unit-4/sprint-3/day-1/assignments/AUTH CONTEXT/auth/src/component/signup.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import { useContext } from "react";
+import {Authcontext} from "../context/authcontext"
+
+const initial = {
+    email : "",
+    password : ""
+}
+const Signup = () => {
+    const [data, setData] = useState(initial);
+    const value = useContext(Authcontext);
+
+    const { log, isAuth, toggleAuth } = value;
+
+    const handleChange = (e) => {
+
+        // console.log(e.target)
+        const { name, value } = e.target
+
+        // console.log(data)
+        setData( {...data, [name]: value}  )
+        // console.log(data)
+        
+    }
+
+    const keep = (event) => (
+        console.log(data),
+        event.preventDefault(),
+        isAuth(data)
+
+    )    
+
+
+    return (
+        <>
+            <h4>Sign Up</h4>
+
+            <form onSubmit={keep}>
+             <input value={data.email}  name="email" onChange={handleChange} type="email" placeholder="username" />
+            <br />
+            <input value={data.password} name = "password" onChange={handleChange} type="password" placeholder="password" />
+            <input  type="submit" value="submit" />    
+
+            </form>
+
+
+        </>
+  )
+
+}
+
+export default Signup;
