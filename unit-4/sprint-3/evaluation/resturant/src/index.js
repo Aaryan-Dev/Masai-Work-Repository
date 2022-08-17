06819cbceee6a8a54c128f7d90f186b5b64705e3
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-17 12:08:35 +0530

    Create index.js

---
+++ unit-4/sprint-3/evaluation/resturant/src/index.js
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import { AppContextProvider } from "./Context/AppContext";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  // <StrictMode>
+
+  <AppContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AppContextProvider>
+  // </StrictMode>
+);
