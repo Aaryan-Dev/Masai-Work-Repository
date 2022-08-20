1fb2fcf035d91a1974902032bcf56b658b7e1047
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-20 23:24:24 +0530

    Create App.js

---
+++ unit-4/sprint-4/day-1/assignments/GITHUB REDUCER/my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// Create a component github, search for users on load of the component
+// Create a reducer and manage fetching with loading, success and failure from there
+// use the dispatchers, ensure you're keeping the loading and error indicators are inside the reducer state
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
