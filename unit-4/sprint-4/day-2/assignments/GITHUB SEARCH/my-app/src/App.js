03f6e148dc9ee62cd10aeab798c2b0ae2f63f3bb
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-20 23:26:07 +0530

    Create App.js

---
+++ unit-4/sprint-4/day-2/assignments/GITHUB SEARCH/my-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+// create a component that allows you to search for users on github
+// use https://api.github.com/search/users api
+// docs
+// on fetching show the results in a list of Card components
+// use axios to fetch data
+// apply pagination, per page limit ( use useEffect to manage these as dependencies )
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
