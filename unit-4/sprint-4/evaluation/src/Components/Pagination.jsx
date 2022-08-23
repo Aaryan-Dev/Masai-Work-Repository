a6d025ce73d5a34dcd1caa36f5a6b09db9e7352a
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-23 17:28:53 +0530

    Create Pagination.jsx

---
+++ unit-4/sprint-4/evaluation/src/Components/Pagination.jsx
@@ -0,0 +1,17 @@
+function createArrayOfSize(n) {
+  return new Array(n).fill(0);
+}
+
+function Pagination({ totalPages, currentPage, handlePageChange }) {
+  console.log(createArrayOfSize(totalPages));
+
+  let pages = createArrayOfSize(totalPages).map((a, i) => (
+    <button onClick={handlePageChange} disabled={i + 1 == currentPage}>
+      {i + 1}{" "}
+    </button>
+  ));
+
+  return <div>{pages}</div>;
+}
+
+export default Pagination;
