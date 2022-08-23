60c779fc0129ead18ec50b9308c740ffccb065b1
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-23 17:29:13 +0530

    Create Home.jsx

---
+++ unit-4/sprint-4/evaluation/src/Routes/Home.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+import { useContext } from "react";
+import { Con } from "../Context/AppContext";
+
+let initial = {
+  email: "",
+  password: "",
+};
+
+function Login() {
+  const [form, setform] = useState(initial);
+  // const [text, setText] = useState("");
+
+  const value = useContext(Con);
+
+  const { isAuth, token, loginUser, logoutUser } = value;
+  console.log(isAuth);
+
+  if (isAuth) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  const handle = (e) => {
+    // console.log(e.target);
+    const { name, value } = e.target;
+    setform({ ...form, [name]: value });
+    console.log(form);
+  };
+
+  const sub = (e) => {
+    e.preventDefault();
+    loginUser(form);
+  };
+
+  return (
+    <div>
+      <form onSubmit={sub}>
+        <div>
+          <label>
+            Email
+            <input
+              onChange={handle}
+              name="email"
+              value={form.email}
+              type="email"
+              placeholder="email"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password
+            <input
+              onChange={handle}
+              value={form.password}
+              name="password"
+              type="password"
+              placeholder="password"
+            />
+          </label>
+        </div>
+        <br />
+        <div>
+          <input type="submit" value="LOG IN" />
+        </div>
+      </form>
+      <div>
+        <Link to="/dashboard">
+          <h3>Dashboard</h3>
+        </Link>
+      </div>
+    </div>
+  );
+}
+export default Login;
