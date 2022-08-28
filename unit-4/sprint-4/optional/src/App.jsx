647346e1e82d3565d3a5722c9f7773132f981d03
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-28 17:42:47 +0530

    Create App.jsx

---
+++ unit-4/sprint-4/optional/src/App.jsx
@@ -0,0 +1,13 @@
+import InputTags from './components/InputTags';
+import './App.css';
+
+function App() {
+
+  return (
+      <div className="App">
+        <InputTags/>
+      </div>
+  );
+}
+
+export default App;
