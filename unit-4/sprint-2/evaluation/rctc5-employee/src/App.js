facd2d6eb00ca8e2c71d46c1caa4576ddc862686
Aryan Dev <nobodygivesafak@gmail.com>
2022-08-28 17:43:06 +0530

    Create App.js

---
+++ unit-4/sprint-2/evaluation/rctc5-employee/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { Dashboard } from "./Components/Dashboard";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
