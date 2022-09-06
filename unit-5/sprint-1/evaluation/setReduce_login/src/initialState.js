4dd2835028e58a0dbe8113305f4526685e265300
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-06 21:14:47 +0530

    Create initialState.js

---
+++ unit-5/sprint-1/evaluation/setReduce_login/src/initialState.js
@@ -0,0 +1,11 @@
+//DO NOT CHANGE THE STRUCTURE OF THIS INITIAL STATE;
+const initialState = {
+  email: "",
+  password: "",
+  isAuth: false,
+  token: "",
+  isLoading: false,
+  isError: false,
+};
+
+export { initialState };
