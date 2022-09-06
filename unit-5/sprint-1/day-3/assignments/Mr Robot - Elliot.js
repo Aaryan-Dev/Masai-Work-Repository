7e5905739fd3792a52b736c58f41da737f36f6dc
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-06 21:14:56 +0530

    Create Mr Robot - Elliot.js

---
+++ unit-5/sprint-1/day-3/assignments/Mr Robot - Elliot.js
@@ -0,0 +1,5 @@
+// Mr.Robot needs an encryption method, so Elliott was there to help him. At the same time, Richard wrote a paper on encryption. Elliott then started writing his algorithms with the help of paper from Richard but he needs your help in implementing. The algorithm is like this:
+
+// Given an input string, the encrypted string will start with the middle character of the string and will be formed henceforth with the middle characters of the left and right substrings (of the middle character of the word) and so on. Take a look at the explanation of the sample test case for better understanding.
+
+//Enter code here
