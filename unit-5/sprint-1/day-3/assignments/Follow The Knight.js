a91211898efd5af4500d5b98ae121ff68fddced6
Aryan Dev <nobodygivesafak@gmail.com>
2022-09-06 21:15:00 +0530

    Create Follow The Knight.js

---
+++ unit-5/sprint-1/day-3/assignments/Follow The Knight.js
@@ -0,0 +1,5 @@
+// You are very good at playing chess since childhood and you knew the rules very much. You have a  10 X 10 chessboard and you want to explore all the possible squares on the board that the knight can be at in exactly N moves.It is placed at (i,j) coordinate initially. For a 10X10 chessboard (1,1) will be the top left corner and (10,10) will be the bottom right corner.
+
+// You can refer the following diagram, the knight can move to any of the squares marked as X in 1 move.
+
+//Enter code here
