e63b86a93f172e2533cec00dd0f8304dff57b1d7
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-11-23 23:00:40 +0530

    Update index.js

--- unit-7/sprint-2/day-2/you/Train_Ticket_Booking/index.js
+++ unit-7/sprint-2/day-2/you/Train_Ticket_Booking/index.js
@@ -1,17 +1,21 @@
 let registered_data = JSON.parse(localStorage.getItem("registered")) || [];
 
-let prior = null;
-
-function priority() {
-  prior = document.getElementById("prior").value;
-  //   console.log(prior);
+let to_station = null;
+function to() {
+  to_station = document.getElementById("to_station").value;
+  // console.log(to_station);
 }
 
-let vaccine = null;
+let from_station = null;
+function from() {
+  from_station = document.querySelector("#from_station").value;
+  // console.log(from_station);
+}
 
-function vaccinity() {
-  vaccine = document.querySelector("#vaccine").value;
-  //   console.log(vaccine);
+let seat = null;
+function book_seat() {
+  seat = document.querySelector("#seat").value;
+  // console.log(seat);
 }
 
 function validate(event) {
@@ -19,12 +23,14 @@ function validate(event) {
 
   let name = document.querySelector("#name").value;
 
+  let date = document.querySelector("#date").value;
+
   let finalname = name.split(" ");
   // *****
   let name_it = finalname.join("");
   name_value = name_it.split("").length;
 
-  console.log(name_value);
+  // console.log(name_value);
 
   let get_id = document.querySelector("#id").value;
 
@@ -44,19 +50,13 @@ function validate(event) {
   // *****
   let get_age = document.querySelector("#age").value;
 
-  let emp = document.querySelector("#emp").checked;
-  let stud = document.querySelector("#std").checked;
-
-  if (prior === "" || vaccine === "" || prior === null || vaccine === null) {
-    alert("select priority and vaccine name");
-  } else if (emp === false && stud === false) {
-    alert("Slelect Designatoin");
+  if (to_station == from_station) {
+    alert("From stastion and To station must be diffrent");
   } else if (get_id == "") {
-    //   console.log(get_id);
     alert("Enter Id");
   } else if (name_value < 4) {
     alert("Name must be atleast 4 characters");
-    console.log(name_value);
+    // console.log(name_value);
   } else if (value == false) {
     alert("Id must be unique");
   } else if (get_age < 18 || get_age > 40) {
@@ -66,12 +66,14 @@ function validate(event) {
       id: get_id,
       name: name,
       age: get_age,
-      designation: emp ? "employee" : "student",
-      priority: prior,
-      vaccine: vaccine,
+      from_station: from_station,
+      to_station: to_station,
+      date: date,
+      seat_type: seat,
     };
+    console.log(object);
     registered_data.push(object);
-
     localStorage.setItem("registered", JSON.stringify(registered_data));
+    alert("Registered");
   }
 }
