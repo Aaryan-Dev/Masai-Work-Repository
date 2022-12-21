f8f2e55a0d4b79a63372d066bc4c6e6c791fc1b3
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-12-21 16:23:11 +0530

    Update db.js

--- unit-7/week-5/word_game/backend/db.js
+++ unit-7/week-5/word_game/backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = mongoose.connect(
-  `mongodb+srv://nobody:YIm4U2ewzxAAl4md@cluster0.il2clvz.mongodb.net/wordGame`
+  `mongodb+srv://nobody:${password}@cluster0.il2clvz.mongodb.net/wordGame`
 );
 
 module.exports = { connection };
