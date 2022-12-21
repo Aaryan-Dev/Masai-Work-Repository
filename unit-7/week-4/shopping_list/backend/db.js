c79229d1070132d102c01cf9ca3b71f819870356
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-12-21 16:25:42 +0530

    Update db.js

--- unit-7/week-4/shopping_list/backend/db.js
+++ unit-7/week-4/shopping_list/backend/db.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = mongoose.connect(
-  `mongodb+srv://nobody:YIm4U2ewzxAAl4md@cluster0.il2clvz.mongodb.net/note`
-  // "mongodb+srv://surya:surya123@cluster0.egl1mnc.mongodb.net/MOCK10"
+  `mongodb+srv://nobody:${password}@cluster0.il2clvz.mongodb.net/note`
 );
 
 module.exports = { connection };
